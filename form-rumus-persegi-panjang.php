a0aca14875584521ba8f91b16245cc284aef88b3
deniper27890 <deniper27890@gmail.com>
2024-09-21 13:48:36 +0700

    tampilan rumus persegi panjang

---
+++ form-rumus-persegi-panjang.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="assets/plugins/bootstrap-5/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+    <!-- konten di sini -->
+    <div class="card mt-5">
+        <div class="card-header bg-info text-white">
+            Rumus Persegi Panjang
+        </div>
+        <div class="card-body">
+        <form>
+            <div class="mb-3">
+                <label class="form-label">Panjang</label>
+                <input type="" class="form-control"  placeholder="isi dengan panjang persegi">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Lebar</label>
+                <input type="" class="form-control"  placeholder="isi dengan lebar persegi">
+            </div>
+            <input type="checkbox" class="btn-check" id="btn-check-2" checked autocomplete="off">
+            <label class="btn btn-primary" for="btn-check-2">kirim</label>
+        </form>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
